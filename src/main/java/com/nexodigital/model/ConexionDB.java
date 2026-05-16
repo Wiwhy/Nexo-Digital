3377e812b5658b308cb4ccb8ef62bad533e7cf07
ConectaCorazonesCorp <conecta.corazones.contacto@gmail.com>
2026-05-16 19:00:01 +0200

    Support DB_ variable names from Railway

--- src/main/java/com/nexodigital/model/ConexionDB.java
+++ src/main/java/com/nexodigital/model/ConexionDB.java
@@ -12,12 +12,12 @@ public class ConexionDB {
     }
 
     public static Connection obtenerConexion() throws SQLException {
-        // Variables de entorno inyectadas por Railway (o valores por defecto para entorno local)
-        String host = getEnv("MYSQLHOST", "mysql_bd_noticias");
-        String port = getEnv("MYSQLPORT", "3306");
-        String database = getEnv("MYSQLDATABASE", "nexo_digital");
-        String user = getEnv("MYSQLUSER", "root");
-        String password = getEnv("MYSQLPASSWORD", "root");
+        // Soporte para los nombres de variables que inyecta Railway
+        String host = getEnv("DB_HOST", getEnv("MYSQLHOST", "mysql_bd_noticias"));
+        String port = getEnv("DB_PORT", getEnv("MYSQLPORT", "3306"));
+        String database = getEnv("DB_NAME", getEnv("MYSQLDATABASE", "railway")); // En Railway la BD suele llamarse "railway"
+        String user = getEnv("DB_USER", getEnv("MYSQLUSER", "root"));
+        String password = getEnv("DB_PASSWORD", getEnv("MYSQLPASSWORD", "root"));
 
         String url = String.format("jdbc:mysql://%s:%s/%s?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC", 
                                    host, port, database);
